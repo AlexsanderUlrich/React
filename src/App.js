b2968e32057004f8219b4aab0d09f2d3c3a690aa
AlexsanderUlrich <alexsanderulrich@gmail.com>
2024-06-05 13:48:17 -0300

    Ajustes para seguir a aula

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import Banner from "./componentes/Banner/Banner";
+import Banner from "./componentes/Banner"
+import CampoTexto from "./componentes/CampoTexto/Index";
 
 function App() {
   return (
-    <Banner/>   
-  );
+    <div className="App">
+      <Banner />
+      <CampoTexto />
+    </div>
+    
+  )
 }
 
 export default App;
---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,14 @@
+import './CampoTexto.css'
+
+function CampoTexto () {
+    return(
+        <div>
+            <label></label>
+            <input 
+            placeholder="Teste"
+            />
+        </div>
+    )
+}
+
+export default CampoTexto;
